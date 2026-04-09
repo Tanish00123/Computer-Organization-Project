f0b4f50f02c520c013df19fce619fc741e707327
vihaan25543-ai <vihaan25543@iiitd.ac.in>
2026-04-09 22:16:42 +0530

    encoder.py
    non significant changes

--- Simulator_files/encoder.py
+++ Simulator_files/encoder.py
@@ -1,15 +1,15 @@
-def to_signed32(val):
+def signed32(val):
     val = val & 0xFFFFFFFF
     if val & 0x80000000:
         val -= 0x100000000
     return val
 
-def to_unsigned32(val):
+def unsigned32(val):
     return val & 0xFFFFFFFF
 
 def write_reg(regs, rd, value):
     if rd != 0:
-        regs[rd] = to_signed32(value)
+        regs[rd] = signed32(value)
 
 class Executor:
     def __init__(self, regs, memory):
@@ -28,8 +28,8 @@ class Executor:
         imm = decoded["imm"]
         opcode = decoded["opcode"]
 
-        r1 = to_signed32(self.regs[rs1])
-        r2 = to_signed32(self.regs[rs2])
+        r1 = signed32(self.regs[rs1])
+        r2 = signed32(self.regs[rs2])
         next_pc = pc + 4   
 
         #R-Type 
@@ -41,21 +41,21 @@ class Executor:
                 write_reg(self.regs, rd, r1 - r2)
 
             elif f3 == "001" and f7 == "0000000":   
-                shamt = to_unsigned32(r2) & 0x1F
+                shamt = unsigned32(r2) & 0x1F
                 write_reg(self.regs, rd, r1 << shamt)
 
             elif f3 == "010" and f7 == "0000000":   
                 write_reg(self.regs, rd, 1 if r1 < r2 else 0)
 
             elif f3 == "011" and f7 == "0000000":   
-                write_reg(self.regs, rd, 1 if to_unsigned32(r1) < to_unsigned32(r2) else 0)
+                write_reg(self.regs, rd, 1 if unsigned32(r1) < unsigned32(r2) else 0)
 
             elif f3 == "100" and f7 == "0000000":   
                 write_reg(self.regs, rd, r1^r2)
 
             elif f3 == "101" and f7 == "0000000":   
-                shamt = to_unsigned32(r2) & 0x1F
-                write_reg(self.regs, rd, to_unsigned32(r1) >> shamt)
+                shamt = unsigned32(r2) & 0x1F
+                write_reg(self.regs, rd, unsigned32(r1) >> shamt)
 
             elif f3 == "110" and f7 == "0000000":   
                 write_reg(self.regs, rd, r1 | r2)
@@ -70,7 +70,7 @@ class Executor:
         elif t == "I":
             #lw with opcode = 0000011
             if opcode == "0000011" and f3 == "010":
-                addr = to_unsigned32(r1 + imm)
+                addr = unsigned32(r1 + imm)
                 write_reg(self.regs, rd, self.mem.lw(addr))
 
             #addi with opcode = 0010011 and funct3 = 000
@@ -79,12 +79,12 @@ class Executor:
 
             #sltiu with opcode = 0010011 and funct3 = 011
             elif opcode == "0010011" and f3 == "011":
-                write_reg(self.regs, rd, 1 if to_unsigned32(r1) < to_unsigned32(imm) else 0)
+                write_reg(self.regs, rd, 1 if unsigned32(r1) < unsigned32(imm) else 0)
 
             #jalr with opcode = 1100111 and funct3 = 000
             elif opcode == "1100111" and f3 == "000":
                 return_addr = pc + 4
-                target = to_unsigned32(r1 + imm) & ~1   # clear LSB
+                target = unsigned32(r1 + imm) & ~1   # clear LSB
                 write_reg(self.regs, rd, return_addr)
                 next_pc = target
 
@@ -95,8 +95,8 @@ class Executor:
         elif t == "S":
             #sw with funct3 = 010
             if f3 == "010":
-                addr = to_unsigned32(r1 + imm)
-                self.mem.sw(addr, to_unsigned32(r2))
+                addr = unsigned32(r1 + imm)
+                self.mem.sw(addr, unsigned32(r2))
             else:
                 raise RuntimeError(f"Unknown S type: funct3={f3} at PC=0x{pc:08X}")
 
@@ -112,9 +112,9 @@ class Executor:
             elif f3 == "101":   # bge (signed)
                 taken = (r1 >= r2)
             elif f3 == "110":   # bltu (unsigned)
-                taken = (to_unsigned32(r1) < to_unsigned32(r2))
+                taken = (unsigned32(r1) < unsigned32(r2))
             elif f3 == "111":   # bgeu (unsigned)
-                taken = (to_unsigned32(r1) >= to_unsigned32(r2))
+                taken = (unsigned32(r1) >= unsigned32(r2))
             else:
                 raise RuntimeError(f"Unknown B-type: funct3={f3} at PC=0x{pc:08X}")
 
@@ -125,10 +125,10 @@ class Executor:
         elif t == "U":
             if opcode == "0110111":   # lui
                 # imm already has lower 12 bits = 0 from decoder
-                write_reg(self.regs, rd, to_signed32(imm))
+                write_reg(self.regs, rd, signed32(imm))
 
             elif opcode == "0010111":  # auipc
-                write_reg(self.regs, rd, to_signed32(pc + imm))
+                write_reg(self.regs, rd, signed32(pc + imm))
 
             else:
                 raise RuntimeError(f"Unknown U-type: opcode={opcode} at PC=0x{pc:08X}")
@@ -145,7 +145,7 @@ class Executor:
 
         return next_pc
 
-    def is_virtual_halt(self, decoded):
+    def virtual_halt(self, decoded):
         if decoded["type"] != "B":
             return False
         if decoded["funct3"] != "000":
