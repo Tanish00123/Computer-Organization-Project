291de6c25b72ca28f6d91265365b9b4355707e17
Rohit Rohilla <rohit25431@iiitd.ac.in>
2026-04-10 00:50:24 +0530

    Create Simulator.py

---
+++ co_2026_evaluation_framework_release/SimpleSimulator/Simulator.py
@@ -0,0 +1,96 @@
+import sys
+from instruction_decoder import instruction_decoder
+from memory import Memory, init_registers
+from encoder import Executor
+
+
+def binary_32(val):
+    return "0b" + format(val & 0xFFFFFFFF, '032b')
+
+
+def trace(pc, regs):
+    line = binary_32(pc)
+    for i in range(32):
+        line += " " + binary_32(regs[i])
+    line += " "
+    print(line)
+
+
+# def run_simu(lines):
+
+#     decoder = instruction_decoder()
+#     decoder.program_memory = lines   # directly assign input
+
+#     memory = Memory()
+#     regs = init_registers()
+
+#     instructions = [int(instr, 2) for instr in decoder.program_memory]
+#     memory.load_program(instructions)
+
+#     executor = Executor(regs, memory)
+
+#     decoded_program = decoder.decode_program()
+
+#     pc = 0
+
+#     while True:
+#         index = pc // 4
+#         decoded = decoded_program[index]
+
+        
+
+#         if executor.is_virtual_halt(decoded):
+           
+#             trace(pc, regs)
+#             break
+
+#         pc = executor.execute(decoded, pc)
+#         trace(pc, regs)
+
+#     dump = memory.dump_data()
+#     for addr, val in dump:
+#         print(f"{addr}:0b{val}")
+
+
+# if __name__ == "__main__":
+#     lines = sys.stdin.read().strip().split('\n')
+#     run_simu(lines)
+
+def run_simu(lines):
+
+    decoder = instruction_decoder()
+    decoder.program_memory = lines
+
+    memory = Memory()
+    regs = init_registers()
+
+    instructions = [int(instr, 2) for instr in decoder.program_memory]
+    memory.load_program(instructions)
+
+    executor = Executor(regs, memory)
+
+    decoded_program = decoder.decode_program()
+
+    pc = 0
+
+    while True:
+        index = pc // 4
+        decoded = decoded_program[index]
+
+        print(f"DEBUG: PC={pc}, type={decoded['type']}, funct3={decoded['funct3']}, imm={decoded['imm']}")
+
+        if executor.is_virtual_halt(decoded):
+            trace(pc, regs)
+            break
+
+        pc = executor.execute(decoded, pc)
+        trace(pc, regs)
+
+    dump = memory.dump_data()
+    for addr, val in dump:
+        print(f"{addr}:0b{val}")
+
+
+if __name__ == "__main__":
+    lines = sys.stdin.read().strip().split('\n')
+    run_simu(lines)
