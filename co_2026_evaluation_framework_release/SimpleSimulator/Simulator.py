9120ae765c32329b22a5f91dc41e6b9dcb700d7f
Rohit Rohilla <rohit25431@iiitd.ac.in>
2026-04-10 00:51:40 +0530

    Update Simulator.py

--- co_2026_evaluation_framework_release/SimpleSimulator/Simulator.py
+++ co_2026_evaluation_framework_release/SimpleSimulator/Simulator.py
@@ -15,47 +15,6 @@ def trace(pc, regs):
     line += " "
     print(line)
 
-
-# def run_simu(lines):
-
-#     decoder = instruction_decoder()
-#     decoder.program_memory = lines   # directly assign input
-
-#     memory = Memory()
-#     regs = init_registers()
-
-#     instructions = [int(instr, 2) for instr in decoder.program_memory]
-#     memory.load_program(instructions)
-
-#     executor = Executor(regs, memory)
-
-#     decoded_program = decoder.decode_program()
-
-#     pc = 0
-
-#     while True:
-#         index = pc // 4
-#         decoded = decoded_program[index]
-
-        
-
-#         if executor.is_virtual_halt(decoded):
-           
-#             trace(pc, regs)
-#             break
-
-#         pc = executor.execute(decoded, pc)
-#         trace(pc, regs)
-
-#     dump = memory.dump_data()
-#     for addr, val in dump:
-#         print(f"{addr}:0b{val}")
-
-
-# if __name__ == "__main__":
-#     lines = sys.stdin.read().strip().split('\n')
-#     run_simu(lines)
-
 def run_simu(lines):
 
     decoder = instruction_decoder()
